8cd7d33595b1cf950e2848ec71c70fc638d655af
hzambrella <504489929@qq.com>
2016-09-26 21:43:51 +0800

    1

--- src/1.5/fetch.go
+++ src/1.5/fetch.go
@@ -1,25 +1,27 @@
 // fetch url
 package main
-import(
+
+import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
-func main(){
-	for _,url:=range os.Args[1:]{
-	resp,err:=http.Get(url)
-	if err!=nil{
-		fmt.Println(err)
-		os.Exit(1)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1) // 无论哪种失败原因，我们的程序都用了os.Exit函数来终止进程，并且返回一个status错误码，其值为1。
+		}
+		// fmt.Println(b) if you  use it,you get number,not html
+		fmt.Printf("%s", b)
 	}
-	b,err:=ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err!=nil{
-		fmt.Println(err)
-		os.Exit(1)// 无论哪种失败原因，我们的程序都用了os.Exit函数来终止进程，并且返回一个status错误码，其值为1。
-	}
-	// fmt.Println(b) if you  use it,you get number,not html 
-	fmt.Printf("%s",b)
-}
 }
