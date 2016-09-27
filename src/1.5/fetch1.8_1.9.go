b80e6c3bade3c8611b75c8ee96e0442d7135d402
hzambrella <504489929@qq.com>
2016-09-27 09:07:24 +0800

    a

--- src/1.5/fetch1.8_1.9.go
+++ src/1.5/fetch1.8_1.9.go
@@ -1,5 +1,6 @@
 // fetch url
 // 修改fetch这个范例，如果输入的url参数没有 http:// 前缀的话，为这个url加上该前缀。你可能会用到strings.HasPrefix这个函数。
+//  修改fetch打印出HTTP协议的状态码，可以从resp.Status变量得到该状态码。
 package main
 
 import (
@@ -22,12 +23,13 @@ func main() {
 			os.Exit(1)
 		}
 		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		defer resp.Body.Close()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1) // 无论哪种失败原因，我们的程序都用了os.Exit函数来终止进程，并且返回一个status错误码，其值为1。
 		}
 		// fmt.Println(b) if you  use it,you get number,not html
 		fmt.Printf("%s", b)
+		fmt.Println(resp.Status)
 	}
 }
