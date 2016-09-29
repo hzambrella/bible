0b43be9554518c4d75138df2124a6e4b53cd1dde
hzambrella <504489929@qq.com>
2016-09-29 10:09:52 +0800

    1

---
+++ src/1.7/server2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"1.7/datastore"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+ var data datastore.Data
+
+func main() {
+	var d datastore.Data
+	d, err := datastore.ParseDataFromFile("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data=d
+	fmt.Println("listen and serve: 8080")
+	http.HandleFunc("/view", view)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	var result []byte
+	result = data.ToJson()
+	what, err := w.Write(result)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(what)
+}
